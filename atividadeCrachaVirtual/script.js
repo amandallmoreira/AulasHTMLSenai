5288e31a72242475a8c62c4c6f300fad0366197f
Amanda Moreira <138037027+amandallmoreira@users.noreply.github.com>
2026-06-18 00:49:54 +0000

    Aula do dia 17/06 e Atividade Crachá Virtual

---
+++ atividadeCrachaVirtual/script.js
@@ -0,0 +1,12 @@
+function imprimirNome() {
+    window.alert("Seu formulário foi enviado");
+    let nome=document.getElementById("nome").value;
+
+    console.log("Seu nome é: "+ nome)
+
+    alert("Aluno cadastrado!\n\n Nome do aluno: " + nome)
+}
+
+// window.alert("Executa o Alert");
+
+// console.log("Executando depois do alert");
