0b19abff3fbc94083c200294e257ced6b18e115a
Amanda Moreira <138037027+amandallmoreira@users.noreply.github.com>
2026-06-18 14:14:31 +0000

    Código JS da Atividade Crachá

--- atividadeCrachaVirtual/script.js
+++ atividadeCrachaVirtual/script.js
@@ -1,10 +1,21 @@
-function imprimirNome() {
+function imprimirCracha() {
     window.alert("Seu formulário foi enviado");
     let nome=document.getElementById("nome").value;
+    let idade=document.getElementById("idade").value;
+    let cargo=document.getElementById("cargo").value;
+    let departamento=document.getElementById("departamento").value;
+    let temAcessoRestrito=document.querySelector("[name=temAcessoRestrito]:checked").value; // O query selector
+    // seleciona o primeiro elemento que seja desse seletor CSS
 
-    console.log("Seu nome é: "+ nome)
+    let cracha="Nome: "+ nome + "\n" +
+    "Idade: " + idade + "\n" + 
+    "Cargo: " + cargo + "\n" + 
+    "Departamento: " + departamento + "\n" + 
+    "Tem acesso restrito: " + temAcessoRestrito;
 
-    alert("Aluno cadastrado!\n\n Nome do aluno: " + nome)
+    console.log(cracha)
+
+    alert(cracha)
 }
 
 // window.alert("Executa o Alert");
