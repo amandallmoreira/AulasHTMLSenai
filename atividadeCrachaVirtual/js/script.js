970cfff71057e73ebacdc6a215773d2bd79221fc
Amanda Moreira <138037027+amandallmoreira@users.noreply.github.com>
2026-06-19 00:36:09 +0000

    Atividades dia 18/06

--- atividadeCrachaVirtual/js/script.js
+++ atividadeCrachaVirtual/js/script.js
@@ -4,8 +4,9 @@ function imprimirCracha() {
     let idade=document.getElementById("idade").value;
     let cargo=document.getElementById("cargo").value;
     let departamento=document.getElementById("departamento").value;
-    let temAcessoRestrito=document.querySelector("[name=temAcessoRestrito]:checked").value; // O query selector
-    // seleciona o primeiro elemento que seja desse seletor CSS
+    let temAcessoRestrito=document.querySelector("[name=temAcessoRestrito]:checked").value; 
+    // O query selector seleciona o primeiro elemento que seja desse seletor CSS
+    
 
     let cracha="Nome: "+ nome + "\n" +
     "Idade: " + idade + "\n" + 
@@ -19,5 +20,5 @@ function imprimirCracha() {
 }
 
 // window.alert("Executa o Alert");
-
 // console.log("Executando depois do alert");
+
